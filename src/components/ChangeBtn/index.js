1f3ef5c6334bd03bcda6a75fdee43a1e534e4bdb
hillsss <2542529198@qq.com>
2022-08-16 23:07:22 +0800

    fix warning

--- src/components/ChangeBtn/index.js
+++ src/components/ChangeBtn/index.js
@@ -7,7 +7,6 @@ export default function SelectTheme (props) {
   }, [])
   const { changeTheme } = props
   const btnWrapper = useRef()
-  const lightBtn = useRef()
   let btns = document.getElementsByClassName('btn');
   const changeBtn = (e, val) => {
     changeTheme(val)
